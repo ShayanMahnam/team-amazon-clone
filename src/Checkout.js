5abdfa920827bdee3546f3b3a635324169d049e5
Farnoosh <91014204+Farnooshmo@users.noreply.github.com>
2023-04-13 12:36:20 +0100

    Add Checkout component

---
+++ src/Checkout.js
@@ -0,0 +1,23 @@
+import "./Checkout.css";
+import CheckoutProduct from "./CheckoutProduct";
+
+function Checkout() {
+  return (
+    <div className="checkout">
+      <div className="checkout__left">
+        <img
+          src="https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668_.jpg"
+          alt=""
+          className="checkout__ad"
+        />
+        <div>
+          <h2 className="checkout__title">Your shopping basket</h2>
+          <CheckoutProduct />
+        </div>
+      </div>
+      <div className="checkout__right"></div>
+    </div>
+  );
+}
+
+export default Checkout;
