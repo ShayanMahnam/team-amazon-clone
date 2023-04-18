d1cae2a6bbcacfa2cd1968b4e2e22f5eb5e155c3
Zobeir <rigi.zobir@yahoo.com>
2023-04-18 21:48:59 +0100

    I created the create context and provide a props to all child

--- src/App.js
+++ src/App.js
@@ -4,18 +4,24 @@ import Home from "./Home";
 import Checkout from "./Checkout";
 import Login from "./Login";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { useState, createContext } from "react";
+
+export const AppContext = createContext;
 
 function App() {
+    const [ basket, setbasket] = useState([]);
   return (
     <div className="App">
-      <Router>
-        <Header />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/Checkout" element={<Checkout />} />
-          <Route path="/Login" element={<Login />} />
-        </Routes>
-      </Router>
+      <AppContext.provider value={{ basket, setbasket}}>
+        <Router>
+          <Header />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/Checkout" element={<Checkout />} />
+            <Route path="/Login" element={<Login />} />
+          </Routes>
+        </Router>
+      </AppContext.provider>
     </div>
   );
 }
